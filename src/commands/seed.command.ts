26a7c5856a2c6ba51e0105922b3f25bc8ceed98d
Ryan de Jonge <naamloosdt@gmail.com>
2026-01-19 09:11:14 +0100

    Add explicit exit after seeding completion
    Explicitly exit after finishing seeding.

--- src/commands/seed.command.ts
+++ src/commands/seed.command.ts
@@ -145,6 +145,7 @@ export class SeedCommand implements CommandModule {
     }
 
     console.log('👍 ', gray.underline(`Finished Seeding`))
+    exit(0) // tell yargs we want to exit explicitly, we're done
   }
 }
 
